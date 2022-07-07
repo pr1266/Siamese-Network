f2ca211dcb3aa107abe3986942d586de13355ff3
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-07-07 22:39:57 +0430

    'init'

---
+++ siamese.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class MyModel(nn.Module):
+
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11,stride=4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            
+            nn.Conv2d(96, 256, kernel_size=5, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+
+            nn.Conv2d(256, 384, kernel_size=3,stride=1),
+            nn.ReLU(inplace=True)
+        )

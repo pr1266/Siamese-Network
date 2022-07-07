b9c5b11fa70c85a197ace894b76f2b2e3c5b276e
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-07-07 23:00:27 +0430

    'main'

---
+++ main.py
@@ -0,0 +1,13 @@
+from .model import SiameseModel
+from .loss import ContrastiveLoss
+from .dataset import SiameseDataset
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+train_data_path = '/data/faces/training/'
+test_data_path = '/data/faces/testing/'
+def main():
+
+    pass
+
+if __name__ == '__main__':
+    main()
